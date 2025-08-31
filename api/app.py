702c9582efbf780bb77f7ae1cb544fcd17f4672f
butros <peteris_veits@yahoo.com>
2025-08-31 21:26:37 +0300

    updated fastAPI schema

--- api/app.py
+++ api/app.py
@@ -1,11 +1,12 @@
-from fastapi import FastAPI
-from schema import PropulsionInput, PropulsionOutput
+from fastapi import FastAPI, status, Response # pyright: ignore[reportMissingImports]
+from api.schema import PropulsionInputBase, PropulsionInputFull, PropulsionOutput
 from scripts.model.get_model import load_train_model
 
 app = FastAPI()
 
-@app.post("/predict", response_model=PropulsionOutput)
-def predict(input_data: PropulsionInput):
+@app.post("/predict", response_model=PropulsionOutput, status_code=status.HTTP_201_CREATED)
+def predict(input_data: PropulsionInputBase):
+    """path/endpoint operation function"""
     the_model, the_preprocessor = load_train_model()
     result = the_model.predict(the_preprocessor.transform(input_data))
     return result
--- api/schema.py
+++ api/schema.py
@@ -1,7 +1,9 @@
-from pydantic import BaseModel, Field
+from pydantic import BaseModel, Field  # pyright: ignore[reportMissingImports]
+from typing import Optional
 
 
-class PropulsionInput(BaseModel):
+
+class PropulsionInputBase(BaseModel):
     windSpeed: float = Field(..., example=4.19)
     windDirection: float = Field(..., example=205.96)
     waveHeight: float = Field(..., example=0.87)
@@ -12,4 +14,28 @@ class PropulsionInput(BaseModel):
     currentDirection: float = Field(..., example=159.73)
     airTemperature: float = Field(..., example=25.65)
     mainEngineMassFlowRate: float = Field(..., example=0.0)
-    
+    fuelEfficiency: Optional[float] = Field(..., example=0)  # remove after testing
+
+
+class PropulsionInputFull(BaseModel):
+    """TODO: update with full variable set.. or use optional in PropulsionInputBase???"""
+    windSpeed: float = Field(..., example=4.19)
+    windDirection: float = Field(..., example=205.96)
+    waveHeight: float = Field(..., example=0.87)
+    waveDirection: float = Field(..., example=136.04)
+    swellHeight: float = Field(..., example=0.06)
+    swellDirection: float = Field(..., example=55.77)
+    currentSpeed: float = Field(..., example=0.07)
+    currentDirection: float = Field(..., example=159.73)
+    airTemperature: float = Field(..., example=25.65)
+    mainEngineMassFlowRate: float = Field(..., example=0.0)
+    fuelEfficiency: Optional[float] = Field(..., example=0)
+
+
+
+class PropulsionOutput(BaseModel):
+    shaftPower: float = Field(..., example=1.575)
+    speedOverGround:float = Field(..., example= 0)
+
+    class Config:
+        from_attributes = True  # remove after testing, SQLAlchemy specific: enable Pydantic data read from database models
